eb534e36d4d574e3b89491c77df0ea0fbe8a3f74
Garren-H <garren382@gmail.com>
2026-02-20 18:56:10 +0200

    copy code from Base._permute! (julia v1.11) for comptability with julia v1.10

--- ext/IFTDualsStaticArraysExt.jl
+++ ext/IFTDualsStaticArraysExt.jl
@@ -2,8 +2,35 @@ module IFTDualsStaticArraysExt
 
 import IFTDuals: make_dual, solve_ift, PromoteToDualArray, SeedDualArray, seed_nested_dual, pvalue, nested_pvalue
 import ForwardDiff: Dual, Partials
-import StaticArrays: MArray, StaticArray, StaticVector, StaticMatrix, similar_type, LU, ldiv! # ldiv! is essentially from LinearAlgebra
+import StaticArrays: SVector, MArray, StaticArray, StaticVector, StaticMatrix, similar_type, LU, ldiv! # ldiv! is essentially from LinearAlgebra
 
+# Additional helpers
+swaprows!(a::AbstractMatrix,i,j) = Base.swaprows!(a,i,j);
+@inline function swaprows!(a::AbstractVector,i,j)
+    @inbounds a[i],a[j] = a[j],a[i]
+    return a
+end
+permuterows!(a::AbstractVecOrMat,p::SVector) = permuterows!(a, MArray(p))
+@inline function permuterows!(a::AbstractVecOrMat,p::AbstractVector{Int}) # copy of Base._permute! (https://github.com/JuliaLang/julia/blob/9aff288d6de6a729b7c71d51642ee30eb6b752ad/base/combinatorics.jl#L151-L165) but handles vectors as well
+    Base.require_one_based_indexing(a, p)
+    p .= .-p
+    @inbounds for i in 1:length(p)
+        p[i] > 0 && continue
+        j = i
+        in = p[j] = -p[j]
+        while p[in] < 0
+            swaprows!(a, in, j)
+            j = in
+            in = p[in] = -p[in]
+        end
+    end
+    a
+end
+function solve_ift!(BNi::AbstractVecOrMat,neg_A::LU) # BNi is already permuted
+    ldiv!(neg_A.L,BNi)
+    ldiv!(neg_A.U,BNi)
+    return BNi
+end
 
 # derivatives.jl overloads
 function make_dual(y::Y, DT::Type{Dual{T,V,N}}, PT::Type{Partials{N,V}}, parts::P) where {T,V,N,NY,Y<:StaticVector{NY,V},P<:AbstractVecOrMat{V}} #maintain S/M Array structure if input is S/M Array
@@ -19,24 +46,20 @@ function solve_ift(BNi::B, neg_A::LU) where {V<:Real,B<:AbstractMatrix{V}} # vec
     # StaticArrays.jl \ implementation uses F.U \ ( F.L \ b[F.p,:] ) when the input is a AbstractMatrix, which is inefficient if b is not a SMatrix (i.e. b[F.p,:] creates a new Matrix, then does 2 triangular solves, allocating new arrays at each step). We instead utilize ldiv!
     b = BNi
     if ismutable(BNi)
-        Base.permuterows!(b,MArray(neg_A.p)) # in-place pivoting, no allocations
+        permuterows!(b,neg_A.p) # in-place pivoting, no allocations
     else
         if !(B <: SubArray) # get a view first, @view(AbstractMatrix,:,:)[neg_A.p.:] allocates a new array. If we have a PartialsArray or some other storage, this is required. 
             b = view(b,:,:) 
         end
         b = b[neg_A.p,:] # apply pivoting, returns a new array, should be minimal allocs
     end
-    ldiv!(neg_A.L,b)
-    ldiv!(neg_A.U,b)
-    return b
+    return solve_ift!(b,neg_A)
 end
 
 function solve_ift(BNi::B, neg_A::LU) where {S,V<:Real,N,B<:Union{StaticArray{S,V,N},AbstractVector{V}}}
     if ismutable(BNi)
-        permute!(BNi,neg_A.p)
-        ldiv!(neg_A.L,BNi)
-        ldiv!(neg_A.U,BNi)
-        return BNi
+        permuterows!(BNi,neg_A.p)
+        return solve_ift!(BNi,neg_A)
     end
     return neg_A \ BNi # when we have a AbstractVector, then BNi[neg_A.p] creates a new SVector, so no additonal allocations occur here. 
 end
